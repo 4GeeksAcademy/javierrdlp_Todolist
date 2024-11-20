f6a6c1dc2dcf80bfff0046f4014fb2033dbd4862
Javier Rodríguez <134724418+javierrdlp@users.noreply.github.com>
2024-11-20 19:29:26 +0000

    Añadido icono de borrado y su función, cambio en el aspecto y configuración de mensaje para cuando no hay tareas

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 import Li from "./Li"
 
 //create your first component
 const Home = () => {
 
-	
-	const [listTodo, setListTodo] = useState( ["Frehas platos", "Tener cosas"])
+	const [listTodo, setListTodo] = useState(["Do housework", "Meet with Marta"])
 	const [newTodo, setNewTodo] = useState("")
 
 	return (
@@ -18,31 +17,33 @@ const Home = () => {
 				<div className="col-4">
 					<h1 className="display-1 text-danger-emphasis mt-5">To Do List...</h1>
 					<div className="input-group flex-nowrap">
-						<input type="text" className="form-control" placeholder="Write something to do later..."
+						<input type="text" className="form-control mb-3" placeholder="Write something to do later..."
 							value={newTodo}
 							onChange={(e) => {
 								setNewTodo(e.target.value)
 							}}
 							onKeyUp={(e) => {
 								if (e.key === "Enter" && newTodo.length > 0) {
-									setListTodo([...listTodo, newTodo])									
+									setListTodo([...listTodo, newTodo])
 									setNewTodo("")
 								}
-							}
-							}
+							}}
 						/>
 					</div>
-					<ul className="list-group">
-						{
-							listTodo.map((value, index) => {
-								return <Li key={index} todo={value} />
-							})
-							
-						}
+					<ul className="list-group list-group-flush">
+						{listTodo.length === 0 ? (
+							<li className="list-group-item">
+								<p className='text-danger-emphasis fw-bold'>No tasks yet, add one...</p>
+							</li>
+						) : (
+							listTodo.map((value, index) => (
+								<Li key={index} toDo={value} index={index} setListToDo={setListTodo} />
+							))
+						)}
 						<li className="list-group-item">
-						<span className="fw-light fst-italic" style={{ fontSize: "12px" }}>{listTodo.length} items left</span>
+							<span className="fw-light fst-italic text-danger-emphasis fw-bold" style={{ fontSize: "12px" }}>{listTodo.length} items left</span>
 						</li>
-					</ul>					
+					</ul>
 				</div>
 				<div className="col-4" />
 			</div>
